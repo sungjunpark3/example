7bac9f25fa3f567024defb8b0d23910a7eea368d
Sungjun Park <mail@sungjunpark.com>
2025-11-29 08:36:12 +0900

    first commit

---
+++ dd.R
@@ -0,0 +1,54 @@
+install.packages("usethis")
+library(usethis)
+
+#### 1. Sign up at GitHub.com ################################################
+
+## If you do not have a GitHub account, sign up here:
+## https://github.com/join
+
+# ----------------------------------------------------------------------------
+
+#### 2. Install git ##########################################################
+
+## If you do not have git installed, please do so: 
+## Windows ->  https://git-scm.com/download/win
+## Mac     ->  https://git-scm.com/download/mac
+## Linux   ->  https://git-scm.com/download/linux
+##             or: $ sudo dnf install git-all
+
+# ----------------------------------------------------------------------------
+
+### 3. Configure git with Rstudio ############################################
+
+## Name, email
+usethis::use_git_config(user.name = "Sungjun Park", user.email = "mail@sungjunpark.com")
+
+## setting: permanent storage
+credentials::credential_helper_set("manager")                 # Windows
+credentials::credential_helper_set("osxkeychain")             # Mac
+credentials::credential_helper_set("store")                   # Linux
+
+## create a personal access token for authentication:
+usethis::create_github_token() 
+## in case usethis version < 2.0.0: usethis::browse_github_token() (or even better: update usethis!)
+
+## copy and paste your token and press enter
+credentials::set_github_pat()
+
+# ----------------------------------------------------------------------------
+
+#### 4. Restart R! ###########################################################
+
+# ----------------------------------------------------------------------------
+
+#### 5. Verify settings ######################################################
+
+usethis::git_sitrep()
+
+## Your username and email should be stated correctly in the output. 
+## Also, the report shoud cotain something like:
+## 'Personal access token: '<found in env var>''
+
+# ----------------------------------------------------------------------------
+
+## THAT'S IT!
